89ad9c05aae28230ad07c82e0a5d7a01c6da9ed6
pushpak90 <pushpakafasate90@gmail.com>
2024-02-22 22:30:30 +0530

    Ang CUrd

---
+++ Pratice/angularCRUD/WebApplication1/Model/TblEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Model
+{
+    public class TblEmployee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id{ get; set; }
+        [StringLength(150)]
+        public string Name { get; set; } = string.Empty;
+        [StringLength(150)]
+        public string LastName { get; set; } = string.Empty;
+        [StringLength(250)]
+        public string Email { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public DateTime Doj { get; set; }
+        [StringLength(20)]
+        public string Gender { get; set; } = string.Empty;
+        public int IsMarried { get; set; }
+        public int IsActive { get; set; }
+        public int DesignationID { get; set; }
+        [NotMapped]
+        public string Designation { get; set; } = string.Empty;
+    }
+}

32ba78c03ee28f52f6336fd16855fa624af8523d
pushpak90 <pushpakafasate90@gmail.com>
2024-03-22 22:14:23 +0530

    SQL Connection

--- Pratice/payment_detail_register/paymentAPI/Models/PaymentDetailContext.cs
+++ Pratice/payment_detail_register/paymentAPI/Models/PaymentDetailContext.cs
@@ -1,6 +1,12 @@
-﻿namespace paymentAPI.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace paymentAPI.Models
 {
-    public class PaymentDetailContext
+    public class PaymentDetailContext : DbContext
     {
+        public PaymentDetailContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<PaymentDetail> paymentDetails { get; set; }
     }
 }
